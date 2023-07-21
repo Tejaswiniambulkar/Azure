774807a84e11807ac712746b6d90741833a6ac48
NovatecStack <121426292+novatecstack@users.noreply.github.com>
2023-07-21 13:44:59 +0530

    Update download-script-and-run.ps1

--- Module-03_Azure_Compute_Services/azure-virtual-machine/Extensions/download-script-and-run.ps1
+++ Module-03_Azure_Compute_Services/azure-virtual-machine/Extensions/download-script-and-run.ps1
@@ -1,4 +1,6 @@
+```
 Set-AzVMCustomScriptExtension -ResourceGroupName "<myResourceGroup>" `
     -VMName "<myVM>" -Name "<myCustomScript>" `
     -FileUri "https://raw.githubusercontent.com/neilpeterson/nepeters-azure-templates/master/windows-custom-script-simple/support-scripts/Create-File.ps1" `
     -Run "Create-File.ps1" -Location "<myVMregion>"
+```
